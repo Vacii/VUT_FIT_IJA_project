7f1fd88d3738f05fcb2f18ffc43604277fd22d24
Vacii <lv.vaclavek@gmail.com>
2022-03-15 13:28:08 +0100

    Added hw1

---
+++ homework1/uml/UMLClassifier.java
@@ -0,0 +1,56 @@
+package ija.homework1.uml;
+
+//Třída reprezentuje klasifikátor v diagramu.
+//Odvozené třídy reprezentují konkrétní podoby klasifikátoru (třída, rozhraní, atribut, apod.)
+public class UMLClassifier extends Element{
+    private boolean definovane;
+
+// ----- Constructors -----
+
+/*
+    UMLClassifier(Název klasifikátoru)
+    Vytvoří instanci třídy Classifier. Instance je uživatelsky definována (je součástí diagramu).
+ */
+    public UMLClassifier(java.lang.String name){
+        super(name);
+        this.definovane = true;
+    }
+
+/*
+    UMLClassifier(Název klasifikátoru, Uživatelsky definován (součástí diagramu))
+    Vytvoří instanci třídy Classifier.
+ */
+    public UMLClassifier(java.lang.String name, boolean isUserDefined){
+        super(name);
+        this.definovane = isUserDefined;
+    }
+
+
+//  ----- Methods -----
+/*
+    forName(Název klasifikátoru)
+    Tovární metoda pro vytvoření instance třídy Classifier pro zadané jméno.
+    Instance reprezentuje klasifikátor, který není v diagramu modelován.
+*/
+    public static UMLClassifier forName(java.lang.String name){
+        return new UMLClassifier(name, false);
+    }
+
+
+/*
+    isUserDefined()
+    Zjišťuje, zda objekt reprezentuje klasifikátor, který je modelován uživatelem v diagramu nebo ne.
+*/
+    public boolean isUserDefined(){
+        return this.definovane;
+    }
+
+/*
+    toString()
+    Vrací řetězec reprezentující klasifikátor v podobě "nazev(userDefined)", kde userDefined je true nebo false.
+*/
+    @Override
+    public java.lang.String toString(){
+        return String.format("%s(%b)", this.getName(), this.definovane);
+    }
+}
