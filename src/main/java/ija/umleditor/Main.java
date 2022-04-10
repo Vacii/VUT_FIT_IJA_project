7f3e54afe100f5d1797e6cc33bcca280cdcd3f87
Vacii <lv.vaclavek@gmail.com>
2022-04-10 15:54:17 +0200

    Basic GUI

--- src/main/java/ija/umleditor/Main.java
+++ src/main/java/ija/umleditor/Main.java
@@ -1,23 +1,28 @@
 package ija.umleditor;
 
+import ija.umleditor.uml.ClassDiagram;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class Main extends Application {
+    public static Scene scene;
+    public static ClassDiagram classDiagram;
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Parent root = FXMLLoader.load(getClass().getResource("MainView.fxml"));
         stage.setTitle("UML Editor");
-        stage.setScene(scene);
+        stage.setScene(new Scene(root));
         stage.show();
     }
 
     public static void main(String[] args) {
+        classDiagram = new ClassDiagram("Diagram");
         launch();
     }
 }
